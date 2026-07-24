dc88073b14d3bf20e327a764687e60f270661d05
Eduardo Benites <tetris.eanacleto@terceiros.mag.com.br>
2026-07-23 23:56:54 -0300

    feat: implementação de formatação personalizada de css

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,11 +1,20 @@
+import React, { useEffect } from 'react';
 import Header from '../Header/Header';
 import styles from './MainLayout.module.css';
 
 export default function MainLayout({ children, showHeader = true }) {
+  
+  // Restaura a cor primária do localStorage sempre que qualquer tela com o MainLayout carregar ou sofrer F5
+  useEffect(() => {
+    const savedColor = localStorage.getItem('@Mag:primaryColour');
+    if (savedColor) {
+      document.documentElement.style.setProperty('--color-primary', savedColor);
+    }
+  }, []);
+
   return (
     <div className={styles.pageContainer}>
       {showHeader && <Header />}
-      
       <main className={styles.mainContent}>
         {children}
       </main>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 export { default as Header } from './components/Header/Header';
 export { default as MainLayout } from './components/MainLayout/MainLayout';
+
+import { FileText, ChevronRight, Check, AlertCircle } from 'lucide-react';
+
+export const Icon = {
+  Document: FileText,
+  ChevronRight: ChevronRight,
+  Check: Check,
+  Alert: AlertCircle
+};
